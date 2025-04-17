95bf3b2f32dbfe1b8e02ee9888640233e3cecd2c
Nurmukhanbet <nurmuhanbet2004@icloud.com>
2025-04-17 23:09:59 +0600

    Integration with auth fully done

--- src/main/java/com/example/Klinik/controller/AuthController.java
+++ src/main/java/com/example/Klinik/controller/AuthController.java
@@ -5,34 +5,50 @@ import com.example.Klinik.model.dto.auth.DoctorRegisterRequest;
 import com.example.Klinik.model.dto.auth.LoginRequest;
 import com.example.Klinik.model.dto.auth.RegisterRequest;
 import com.example.Klinik.service.AuthService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-@RestController
+@Controller
 @AllArgsConstructor
 @RequestMapping("/api/auth")
 public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/login")
-    public AuthResponse login(@RequestBody LoginRequest request) {
-        return authService.login(request);
+    public String login(@ModelAttribute LoginRequest request, HttpServletResponse response) {
+        cookieOperation(authService.login(request), response);
+        return "redirect:/api/pages/home";
     }
 
     @PostMapping("/register")
-    public AuthResponse register(@RequestBody RegisterRequest request) {
-        return authService.register(request);
+    public String register(@ModelAttribute RegisterRequest request, HttpServletResponse response) {
+        cookieOperation(authService.register(request), response);
+        return "redirect:/api/pages/home";
     }
 
     @PostMapping("/doctor/register")
-    public AuthResponse registerDoctor(
-            @RequestPart DoctorRegisterRequest request,
-            @RequestPart MultipartFile image) {
-        return authService.register(request, image);
+    public String registerDoctor(
+            @ModelAttribute DoctorRegisterRequest request,
+            @RequestParam MultipartFile image,
+            HttpServletResponse response
+    ) {
+        cookieOperation(authService.register(request, image), response);
+        return "redirect:/api/pages/home";
+    }
+
+    private void cookieOperation(AuthResponse authResponse, HttpServletResponse response) {
+        Cookie tokenCookie = new Cookie("access_token", authResponse.getToken());
+
+        tokenCookie.setMaxAge(24 * 60 * 60);
+        tokenCookie.setPath("/");
+
+        response.addCookie(tokenCookie);
     }
 }
--- src/main/java/com/example/Klinik/controller/PageController.java
+++ src/main/java/com/example/Klinik/controller/PageController.java
@@ -1,15 +1,24 @@
 package com.example.Klinik.controller;
 
+import com.example.Klinik.model.dto.auth.DoctorRegisterRequest;
+import com.example.Klinik.model.dto.auth.LoginRequest;
+import com.example.Klinik.model.dto.auth.RegisterRequest;
+import com.example.Klinik.model.dto.doctor.DoctorResponse;
+import com.example.Klinik.service.DoctorService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @AllArgsConstructor
 @RequestMapping("/api/pages")
 public class PageController {
+
+    private final DoctorService doctorService;
 
     @GetMapping("/not-found")
     public String notFound(Model model) {
@@ -48,11 +57,31 @@ public class PageController {
 
     @GetMapping("/our-doctor")
     public String team(Model model) {
+        List<DoctorResponse> doctors = doctorService.all();
+        model.addAttribute("doctors", doctors);
         return "team";
     }
 
     @GetMapping("/testimonial")
     public String testimonial(Model model) {
         return "testimonial";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        model.addAttribute("loginRequest", new LoginRequest());
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register(Model model) {
+        model.addAttribute("registerRequest", new RegisterRequest());
+        return "register";
+    }
+
+    @GetMapping("/doctor-register")
+    public String doctorRegister(Model model) {
+        model.addAttribute("doctorRequest", new DoctorRegisterRequest());
+        return "doctor-register";
     }
 }
